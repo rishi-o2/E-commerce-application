8e0a408027b581c9806b32444f17947832008494
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-06-14 00:33:03 +0530

    Update CategoryProduct.js

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -14,7 +14,7 @@ const CategoryProduct = () => {
   const getPrductsByCat = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/product-category/${params.slug}`
+        `https://e-commerce-application-6bev.onrender.com/api/v1/product/product-category/${params.slug}`
       );
       setProducts(data?.products);
       setCategory(data?.category);
@@ -38,7 +38,7 @@ const CategoryProduct = () => {
                   key={p._id}
                 >
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://e-commerce-application-6bev.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />

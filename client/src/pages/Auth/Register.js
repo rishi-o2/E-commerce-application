dd2a17656668c7b67026227ea7d01112b886cc98
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-06-14 00:22:55 +0530

    Update Register.js

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -17,14 +17,17 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/register", {
-        name,
-        email,
-        password,
-        phone,
-        address,
-        answer,
-      });
+      const res = await axios.post(
+        "https://e-commerce-application-6bev.onrender.com/api/v1/auth/register",
+        {
+          name,
+          email,
+          password,
+          phone,
+          address,
+          answer,
+        }
+      );
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
         navigate("/login");

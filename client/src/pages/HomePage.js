ea9e079d41fa651b5bc06f1b6f45c82d1ded20ea
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-06-14 00:33:29 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -25,7 +25,9 @@ const HomePage = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get(
+        "https://e-commerce-application-6bev.onrender.com/api/v1/category/get-category"
+      );
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -42,13 +44,14 @@ const HomePage = () => {
   const getAllProducts = async () =>{
     try {
       setLoading(true);
-      const { data } = await axios.get(`/api/v1/product/get-product`,
-      {
-        headers: {
-          Authorization: auth?.token,
-        },
-
-      });
+      const { data } = await axios.get(
+        `https://e-commerce-application-6bev.onrender.com/api/v1/product/get-product`,
+        {
+          headers: {
+            Authorization: auth?.token,
+          },
+        }
+      );
       setLoading(false);
       setProducts(data.products);
     } catch (error) {
@@ -60,13 +63,13 @@ const HomePage = () => {
   //getTOtal COunt
   const getTotal = async () => {
     try {
-      const { data } = await axios.get("/api/v1/product/product-count",
-      {
-        headers: {
-          Authorization: auth?.token,
-        },
-
-      }
+      const { data } = await axios.get(
+        "https://e-commerce-application-6bev.onrender.com/api/v1/product/product-count",
+        {
+          headers: {
+            Authorization: auth?.token,
+          },
+        }
       );
       setTotal(data?.total);
     } catch (error) {
@@ -82,7 +85,9 @@ const HomePage = () => {
   const loadMore = async () => {
     try {
       setLoading(true);
-      const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+      const { data } = await axios.get(
+        `https://e-commerce-application-6bev.onrender.com/api/v1/product/product-list/${page}`
+      );
       setLoading(false);
       setProducts([...products, ...data?.products]);
     } catch (error) {
@@ -112,11 +117,13 @@ const HomePage = () => {
   //get filterd product
   const filterProduct = async () => {
     try {
-      const { data } = await axios.post("/api/v1/product/product-filters", {
-        checked,
-        radio,
-        
-      });
+      const { data } = await axios.post(
+        "https://e-commerce-application-6bev.onrender.com/api/v1/product/product-filters",
+        {
+          checked,
+          radio,
+        }
+      );
       setProducts(data?.products);
     } catch (error) {
       console.log(error);
@@ -163,7 +170,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" style={{ width: "18rem" }}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`https://e-commerce-application-6bev.onrender.com/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
@@ -175,16 +182,25 @@ const HomePage = () => {
                   <p className="card-text"> $ {p.price}</p>
                   <button
                     className="btn btn-primary ms-1"
-                    onClick={() => {navigate(`/product/${p.slug}`)}}
+                    onClick={() => {
+                      navigate(`/product/${p.slug}`);
+                    }}
                   >
                     More Details
                   </button>
-                  <button className="btn btn-secondary ms-1"
-                  onClick={()=>{
-                    setCart([...cart,p]);
-                    localStorage.setItem('cart',JSON.stringify([...cart,p]))
-                    toast.success('Item added to cart')
-                  }}>ADD TO CART</button>
+                  <button
+                    className="btn btn-secondary ms-1"
+                    onClick={() => {
+                      setCart([...cart, p]);
+                      localStorage.setItem(
+                        "cart",
+                        JSON.stringify([...cart, p])
+                      );
+                      toast.success("Item added to cart");
+                    }}
+                  >
+                    ADD TO CART
+                  </button>
                 </div>
               </div>
             ))}

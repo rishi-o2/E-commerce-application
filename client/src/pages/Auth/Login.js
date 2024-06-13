52828d044909b6c444d150da771427dad2152f37
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-06-14 00:22:25 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -19,10 +19,13 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        "https://e-commerce-application-6bev.onrender.com/api/v1/auth/login",
+        {
+          email,
+          password,
+        }
+      );
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
         setAuth({

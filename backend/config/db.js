73b9c2d41761d375ca471118a30748347e9fea8d
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-06-13 20:35:09 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,18 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://rishiind94:rishi123@cluster0.gvbv7jf.mongodb.net/?retryWrites=true&w=majority", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("Connection successful...");
+    await mongoose.connect(
+      "mongodb+srv://rishiind94:l1IWuKt6WFPTm6Xj@cluster0.fcz51nm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error("Connection failed:", err);
+    console.error(err.message);
+    process.exit(1);
   }
 };
 
